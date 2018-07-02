bee42c7d8349b9fddd217e50f7f74398b4b7b242
etienne <xdze2.me@gmail.com>
2018-07-02 23:14:16 +0200

    clean and separated test script

--- graphbuilder.ts
+++ graphbuilder.ts
@@ -2,14 +2,25 @@
 /*
 -- Graph Builder --
 
-- input: list of doi, number of generation to grow, selection rule
-- output: svg graph, metadata
-
+- input: list of doi, number of generation to grow, and a selection rule
+- output:
+  - a graph i.e. Object like doi:[references]
+  - svg graph, metadata
  */
 
 import * as bibistore from './bibistore'
 
-console.log('-- hello --')
+
+export function init_graph( doi_list: string[] ){
+  /* Create a graph starting at gen0 */
+  let graph = {}
+  doi_list.forEach(
+    function(doi){
+      doi = doi.toLowerCase().trim()
+      graph[doi] = {gen:0, citedby:[]}
+  })
+  return graph
+}
 
 function lastGen(graph){
   /* Return the number of the last generation in the graph */
@@ -18,29 +29,17 @@ function lastGen(graph){
   return keys.reduce( findMax, 0 )
 }
 
-
-function init_graph( doi_list: string[] ){
-  /* Create a graph starting at gen0 */
-  let graph = {}
-  doi_list.forEach(
-    function(doi){
-    doi = doi.toLowerCase().trim()
-    graph[doi] = {gen:0, citedby:[]}
-  })
-  return graph
-}
-
-
-// - function growOneGen( graph ){}
-function growOneGen( graph ){
+export function growOneGen( graph ){
   /* Fetch the references for the last-generation nodes
      and expand the graph one generation
   */
+
+  // Look for the unknown references:
   const graph_lastgen = lastGen(graph)
 
   const keys = Object.keys(graph);
   const lastgen_doi = keys.filter( key => graph[key].gen==graph_lastgen )
-  console.log('lastgen doi:', lastgen_doi)
+  console.log('lastgen doi:', lastgen_doi.length)
 
   const stored = bibistore.stored_doi_list()
   const missing = lastgen_doi.filter( doi => bibistore.get(doi) == null )
@@ -48,6 +47,7 @@ function growOneGen( graph ){
 
   // TODO: if missing empty?
 
+  // Perform the query for the unknown references and expand the graph:
   return bibistore.query( missing ).then( function(){
     for (let doi of lastgen_doi) {
         const metadata = bibistore.get(doi)
@@ -56,9 +56,9 @@ function growOneGen( graph ){
           metadata.reference
               .forEach(function(info){
 
+                // Add node to the graph:
                 if( info['DOI'] ){
                   const refdoi = info['DOI'].trim().toLowerCase()
-                  // Add node to the graph:
                   if( graph[refdoi] ){ graph[refdoi].citedby.push(doi) }
                   else{ graph[refdoi] = {gen: graph_lastgen+1, citedby: [doi]} }
                 }
@@ -74,13 +74,12 @@ function growOneGen( graph ){
 
 }
 
+
 // Test:
+/*
 let doi_list = ['10.1103/PhysRevA.62.012306']
 
-let graph =  init_graph( doi_list )
+let graph = init_graph( doi_list )
 console.log('graph init: ', graph)
 console.log( "id lastgen: ", lastGen(graph) )
-
-growOneGen( graph )
-  .then( graph=>growOneGen( graph ) )
-  .then( graph=>console.log('graph then', graph) )
+*/
---
+++ graphbuilder_test.ts
@@ -0,0 +1,19 @@
+
+/*
+-- Graph Builder --
+
+- input: list of doi, number of generation to grow, and a selection rule
+- output:
+  - a graph i.e. Object like doi:[references]
+  - svg graph, metadata
+ */
+
+import * as graphbuilder from './graphbuilder'
+
+
+let doi_list = ['10.1103/PhysRevA.62.012306']
+let graph = graphbuilder.init_graph( doi_list )
+
+graphbuilder.growOneGen( graph )
+  .then( graph=>graphbuilder.growOneGen( graph ) )
+  .then( graph=>console.log('graph then', graph) )
