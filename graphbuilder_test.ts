87a41f00b0d4a7f9bcdd73276929cb777dfcfdba
Etienne Navarro <iopzae@gmail.com>
2018-07-05 18:21:06 +0200

    empty chunk

--- graphbuilder_test.ts
+++ graphbuilder_test.ts
@@ -15,11 +15,11 @@ let doi_list = ['10.1103/PhysRevA.62.012306']
 let graph = graphbuilder.init_graph( doi_list )
 
 graphbuilder.growOneGen( graph )
-  .then( graph=>graphbuilder.growOneGen( graph ) )
+/*  .then( graph=>graphbuilder.growOneGen( graph ) )
   .then( function(graph){
     let nodes = graphbuilder.select_minimumcited(graph, 4)
     console.log('nodes selected', nodes)
 
     console.log('upward', graphbuilder.upward_graph( graph, nodes ) )
 
-  })
+}) */
